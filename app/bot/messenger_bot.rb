1ca41df18d42c95decbc09f91a99e91f1ad5fcec
Matt Morgante <m.morgante@telegraafmedia.nl>
2017-04-19 17:01:28 +0200

    testing veggie with text

--- app/bot/messenger_bot.rb
+++ app/bot/messenger_bot.rb
@@ -137,6 +137,7 @@ end
 
 def ask_first_question postback
   postback.reply(
+    text: 'How many servings of meat did you eat last week? A serving is about the size of a deck of cards',
     quick_replies: [
       { content_type: 'text', title: '1', payload: 'VEGGIE' },
       { content_type: 'text', title: '2', payload: 'MEAT_ONE' },
