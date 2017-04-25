2ed5ac4710d5b353d58fd24509bf9d33fb0dcf9e
Matt Morgante <m.morgante@telegraafmedia.nl>
2017-04-25 14:58:43 +0200

    changed bot model

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,15 +19,15 @@ class User < ApplicationRecord
 		
   	total = impact_score
 		case total
-		when 0..3
+		when 0..2
 		  "You scored a 5 out of 5! Over the course of an average day, your food accounts for #{analitics[0][0]} liters of Water and #{analitics[0][1]} grams of Carbon Dioxide"
-		when 3..5
+		when 2..4
 		  "You scored a 4 out of 5! Over the course of an average day, your food accounts for #{analitics[1][0]} liters of Water and #{analitics[1][1]} grams of Carbon Dioxide"
-		when 5..6
+		when 4..5
 		  "You scored a 3 out of 5! Over the course of an average day, your food accounts for #{analitics[2][0]} liters of Water and #{analitics[2][1]} grams of Carbon Dioxide"
-		when 6..7
+		when 5..6
 		  "You scored a 2 out of 5! Over the course of an average day, your food accounts for #{analitics[3][0]} liters of Water and #{analitics[3][1]} grams of Carbon Dioxide"
-		when 7..8
+		when 6..8
 		  "You scored a 1 out of 5! Over the course of an average day, your food accounts for #{analitics[4][0]} liters of Water and #{analitics[4][1]} grams of Carbon Dioxide"
 		else
 		  'Sorry we are not able to calculate your score!'
